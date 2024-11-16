3ff65ddf84894160dc53296ec353a70132ae2e6e
Sepidehmasoumi <sepidem633@gmail.com>
2024-11-16 21:58:05 +0330

    tamrin1

---
+++ tamrin2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace tamrin2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnshowmember = new System.Windows.Forms.Button();
+            this.btnbookinfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnshowmember
+            // 
+            this.btnshowmember.Location = new System.Drawing.Point(68, 194);
+            this.btnshowmember.Name = "btnshowmember";
+            this.btnshowmember.Size = new System.Drawing.Size(185, 29);
+            this.btnshowmember.TabIndex = 0;
+            this.btnshowmember.Text = "نمایش اطلاعات عضو";
+            this.btnshowmember.UseVisualStyleBackColor = true;
+            this.btnshowmember.Click += new System.EventHandler(this.btnshowmember_Click);
+            // 
+            // btnbookinfo
+            // 
+            this.btnbookinfo.Location = new System.Drawing.Point(68, 119);
+            this.btnbookinfo.Name = "btnbookinfo";
+            this.btnbookinfo.Size = new System.Drawing.Size(190, 29);
+            this.btnbookinfo.TabIndex = 1;
+            this.btnbookinfo.Text = "نمایش اطلاعات کتاب";
+            this.btnbookinfo.UseVisualStyleBackColor = true;
+            this.btnbookinfo.Click += new System.EventHandler(this.btnbookinfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 344);
+            this.Controls.Add(this.btnbookinfo);
+            this.Controls.Add(this.btnshowmember);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnshowmember;
+        private System.Windows.Forms.Button btnbookinfo;
+    }
+}
+
---
+++ tamrin2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tamrin2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnbookinfo_Click(object sender, EventArgs e)
+        {
+            book book = new book
+            {
+                title = "از قیطریه تا اورنج کانتی",
+                author = "حمیدرضا صدر",
+                year = int.Parse("1400")
+            };
+            book.printinfo();
+        }
+
+        private void btnshowmember_Click(object sender, EventArgs e)
+        {
+            member mb = new member
+            {
+                name = "یارا",
+                memeberid = 112,
+                phone = "09331109593"
+            };
+            mb.prinmemberinfo();
+        }
+    }
+}
+    public class book
+    {
+    public string title;
+    public string author;
+    public int year;
+    public void printinfo()
+    {
+        MessageBox.Show("اطلاعات کتاب \n" + "عنوان کتاب:" + title + "\n" + " نویسنده: "  + author + "\n" + "سال انتشار:" + year + "\n");
+    }
+    }
+     public class member
+     {
+    public string name;
+    public int memeberid;
+    public string phone;
+    public void prinmemberinfo()
+    {
+        MessageBox.Show("اطلاعات کاربر\n " + "اسم کاربر:"  + name  + "\n"  +   "ایدی کاربر:"  + memeberid + "\n" + "شماره تماس:" + phone); 
+    }
+     }
+     
